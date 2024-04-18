bbb10478b67e23f20c361eba8a4a0dfd713231f4
Pavlo Betsa <bigworld2303@gmail.com>
2024-04-18 17:28:11 +0300

    Added Book entity, repository and service

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,38 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.math.BigDecimal;
+import org.example.config.AppConfig;
+import org.example.model.Book;
+import org.example.service.BookService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Book book1 = new Book();
+        book1.setAuthor("F. Gerbert");
+        book1.setTitle("Dune");
+        book1.setPrice(BigDecimal.valueOf(200));
+        book1.setDescription("Great");
+        book1.setId(1L);
+        book1.setIsbn("9v37h");
+        book1.setCoverImage("f873ghf");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Book book2 = new Book();
+        book2.setAuthor("F. Gerbert");
+        book2.setTitle("Mua D'ib");
+        book2.setPrice(BigDecimal.valueOf(250));
+        book2.setDescription("Cool");
+        book2.setId(2L);
+        book2.setIsbn("9v37h2");
+        book2.setCoverImage("f873gh3f");
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        BookService bookService = context.getBean(BookService.class);
+
+        bookService.save(book1);
+        bookService.save(book2);
+
+        System.out.println(bookService.findAll());
     }
 }
